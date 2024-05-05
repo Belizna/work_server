96fc8dee8041f61a5984805d358d5d59a496488c
Belizna <yan.targarien@mail.ru>
2024-05-05 13:38:36 +0300

    микрооптимизация

--- index.js
+++ index.js
@@ -19,7 +19,7 @@ import { get_early_payment, add_early_payment,edit_early_payment,delete_early_pa
 import {other_books_add,other_books_get,other_books_edit,other_books_delete} from './controller/OrherBooksController.js'
 import { get_heresy_books, edit_heresy_books,delete_heresy_books,add_heresy_books } from './controller/heresy_horusConstroller.js'
 import { delete_games, edit_games,add_games,get_games } from './controller/gamesController.js'
-import { book_static, repair_static, credit_static, games_static, salary_chart, hobby_static, main_static, main_static_refactor } from './controller/chartsController.js'
+import { book_static, repair_static, credit_static, games_static, salary_chart, hobby_static, main_static} from './controller/chartsController.js'
 import { get_write_books, edit_write_books, add_write_books, delete_write_books } from './controller/writeBooksController.js'
 import { bonus_get , bonus_add, bonus_delete, bonus_edit } from './controller/weekendController.js'
 import { salary_add, salary_delete, salary_edit, salary_get } from './controller/SalaryController.js'
@@ -127,7 +127,6 @@ app.get('/games/static', games_static)
 app.get('/weekend/work/charts', salary_chart)
 app.get('/hobby/static/', hobby_static)
 app.get('/main/static/:year', main_static)
-app.get('/main/static2/:year', main_static_refactor)
 app.get("/repair/static", repair_static)
 
 
